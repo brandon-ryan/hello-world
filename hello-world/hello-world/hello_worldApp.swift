a2b89d41643ee2f45d7ae0a19269573c79b29e8a
Brandon Ryan <brandon.ryan@logmein.com>
2021-02-10 21:47:47 -0500

    adding ios app

---
+++ hello-world/hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Brandon Ryan on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
